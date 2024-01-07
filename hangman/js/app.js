6a9305b5e3df5c15c07b97796c7e89b6612b2ab1
Alex <calium_by@mail.ru>
2024-01-07 18:32:09 +0300

    feat: add words data base and implement random question show on the page

--- hangman/js/app.js
+++ hangman/js/app.js
@@ -1,45 +1,101 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //render page template
-  const letters = [];
-  for (let s = 65; s <= 90; s++) {
-    letters.push(String.fromCharCode(s));
-  }
+document.addEventListener('DOMContentLoaded', () => {
+    //render page template
+    const letters = [];
+    for (let s = 65; s <= 90; s++) {
+        letters.push(String.fromCharCode(s));
+    }
 
-  function renderApp() {
-    const app = document.createElement("main");
-    app.className = "main";
+    const DB = [
+        {
+            question:
+                'a public statement that is made without giving proof, accusing somebody of doing something that is wrong or illegal',
+            answer: 'allegation',
+        },
+        {
+            question:
+                'the machines and electronic parts in a computer or other electronic system',
+            answer: 'hardware',
+        },
+        {
+            question: 'the country where a person was born',
+            answer: 'homeland',
+        },
+        {
+            question: 'a baby or very young child',
+            answer: 'infant',
+        },
+        {
+            question:
+                'person or company from whom you rent a room, a house, an office, etc.',
+            answer: 'landlord',
+        },
+        {
+            question: 'the great size or importance of something',
+            answer: 'magnitude',
+        },
+        {
+            question: 'the act of cutting and gathering crops',
+            answer: 'harvest',
+        },
+        {
+            question:
+                'a rude way to refer to somebody who you think is very stupid',
+            answer: 'idiot',
+        },
+        {
+            question: 'a young person or a child',
+            answer: 'youngster',
+        },
+        {
+            question:
+                'a building where large quantities of goods are stored, especially before they are sent to shops to be sold',
+            answer: 'warehouse',
+        },
+    ];
 
-    const gallowContainer = document.createElement("section");
-    gallowContainer.className = "gallow-container";
-    app.append(gallowContainer);
+    function renderApp() {
+        const app = document.createElement('main');
+        app.className = 'main';
 
-    const textArea = document.createElement("section");
-    textArea.className = "text-area";
-    app.append(textArea);
+        const gallowContainer = document.createElement('section');
+        gallowContainer.className = 'gallow-container';
+        app.append(gallowContainer);
 
-    const keyBoard = document.createElement("section");
-    keyBoard.className = "key-board";
-    showLetterButtons(letters, keyBoard);
+        const textArea = document.createElement('section');
+        textArea.className = 'text-area';
+        app.append(textArea);
 
-    app.append(keyBoard);
+        const keyBoard = document.createElement('section');
+        keyBoard.className = 'key-board';
+        showLetterButtons(letters, keyBoard);
 
-    document.body.append(app);
-  }
+        app.append(keyBoard);
+        showQuestion(textArea);
+        document.body.prepend(app);
+    }
 
-  //Generate letters array
+    //Generate letters array
 
-  function showLetterButtons(arr, parentNode) {
-    arr.forEach((letter) => {
-      parentNode.append(createLetterButtons(letter, parentNode));
-    });
-  }
+    function showLetterButtons(arr, parentNode) {
+        arr.forEach((letter) => {
+            parentNode.append(createLetterButtons(letter, parentNode));
+        });
+    }
 
-  function createLetterButtons(letter) {
-    const button = document.createElement("button");
-    button.textContent = letter;
-    button.className = "letter-button";
-    return button;
-  }
+    function createLetterButtons(letter) {
+        const button = document.createElement('button');
+        button.textContent = letter;
+        button.className = 'letter-button';
+        return button;
+    }
 
-  renderApp();
+    function showQuestion(parentNode) {
+        let index = Math.floor(Math.random() * (DB.length));
+        if (index > DB.length) {
+            index = DB.length - 1;
+        }
+        parentNode.textContent = DB[index].question;
+    }
+
+    renderApp();
 });
