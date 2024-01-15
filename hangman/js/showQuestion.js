f222148a862941f7e13dbe7b746234d8aba5d4b2
ulikemyway1 <calium@outlook.com>
2024-01-15 16:05:50 +0300

    feat: improve random function to avoid repeating questions in page reload

--- hangman/js/showQuestion.js
+++ hangman/js/showQuestion.js
@@ -3,13 +3,15 @@ import { showWordLine } from "./showWordLine.js";
 import { settings } from "./settings.js";
 export function showQuestion(parentNode) {
     let index = settings.currentWordID;
-    while (index === settings.currentWordID) {
-        index = Math.floor(Math.random() * DB.length);
+    let localIndex = localStorage.getItem('localIndex_ulikemyway');
+    while (index === settings.currentWordID || index === +localIndex) {
+        index = Math.round(Math.random() * DB.length);
     }
-    if (index > DB.length) {
+    if (index >= DB.length) {
         index = DB.length - 1;
     }
     settings.currentWordID = index;
+    localStorage.setItem('localIndex_ulikemyway', index);
     parentNode.textContent = DB[index].question;
     showWordLine(parentNode.previousSibling, index)
 }
