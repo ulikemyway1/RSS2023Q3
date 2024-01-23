d50cb7482b18db887394e97c1ff287d66e41b2f1
ulikemyway1 <calium@outlook.com>
2024-01-23 17:59:11 +0300

    init: start nonogram task. create folders

---
+++ nonograms/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  entry: { index: path.resolve(__dirname, "js", "app.js") },
+  output: {
+    path: path.resolve(__dirname, "build")
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    }),
+    new CopyPlugin({
+        patterns: [
+          { from: "img", to: "img", noErrorOnMissing: true },
+        ],
+      })
+  ]
+};
