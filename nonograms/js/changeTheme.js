8cd340783b9138662c51f8d3a6e1642c7a249fc9
ulikemyway1 <calium@outlook.com>
2024-02-03 15:05:26 +0300

    feat: add dark-theme

--- nonograms/js/changeTheme.js
+++ nonograms/js/changeTheme.js
@@ -1,4 +1,5 @@
 import { changeThemeBtnSlider } from "./renderMainApp.js";
 export function changeTheme() {
     changeThemeBtnSlider.classList.toggle('switched');
-}
+    document.body.classList.toggle('dark-theme');
+}   
