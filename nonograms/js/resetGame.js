af5d1c1819a17b3bbfcdc3def07b4226857f1ddd
ulikemyway1 <calium@outlook.com>
2024-02-03 13:52:34 +0300

    feat: reset timer when reset game

--- nonograms/js/resetGame.js
+++ nonograms/js/resetGame.js
@@ -1,13 +1,16 @@
 import { allCells, boardIsBlocked } from './appState.js';
 import { pickedCells } from './appState.js';
 import { gameFieldClickHandler } from './gameFieldClickHandler.js';
-import { gameField, saveGameBtn } from './renderMainApp.js';
+import { gameField, saveGameBtn, timer } from './renderMainApp.js';
 export function resetGame() {
     allCells.forEach((cell) => {
         cell.classList.remove('picked', 'crossed');
     });
     pickedCells.clear();
     saveGameBtn.disabled = false;
+    timer.stop();
+    timer.reset();
+    timer.render();
     if (boardIsBlocked) {
         gameField.addEventListener('click', gameFieldClickHandler);
     }
