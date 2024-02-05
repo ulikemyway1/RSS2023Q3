e69a3a7a0fb096dbcf1ab67198109ace5575f9b0
ulikemyway1 <calium@outlook.com>
2024-02-05 16:55:56 +0300

    fix: start timer when cross a cell

--- nonograms/js/crossCell.js
+++ nonograms/js/crossCell.js
@@ -1,19 +1,23 @@
-import { pickedCells } from "./appState";
+import { isSolve, pickedCells } from "./appState";
 import { crossedSoundPlay, emptyCellPlay } from "./prepareSounds";
+import { timer } from "./renderMainApp";
 export function crossCell(e) {
     const cell = e.target;
     e.preventDefault();
     if (e.target.classList.contains('cell')) {
-    cell.classList.remove('picked');
-    cell.classList.toggle('crossed');
-    
-    if (pickedCells.has(cell)) {
-        pickedCells.delete(cell);
+        if (!timer.getStatus() && !isSolve[0]) {
+            timer.start();
+        }
+        cell.classList.remove('picked');
+        cell.classList.toggle('crossed');
+
+        if (pickedCells.has(cell)) {
+            pickedCells.delete(cell);
+        }
+        if (!cell.classList.contains('crossed') && !cell.classList.contains('picked')) {
+            emptyCellPlay();
+        } else {
+            crossedSoundPlay();
+        }
     }
-    if (!cell.classList.contains('crossed') && !cell.classList.contains('picked')) {
-        emptyCellPlay();
-    } else {
-        crossedSoundPlay();
-    }
- }
 }
