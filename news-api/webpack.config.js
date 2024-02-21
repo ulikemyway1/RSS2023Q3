e42f2cf97f7099feaaa154b4530a1f0dc194ebc6
ulikemyway1 <calium@outlook.com>
2024-02-21 13:47:47 +0300

    feat: auto run eslint on webpack run

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -34,6 +35,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
     ],
 };
 
