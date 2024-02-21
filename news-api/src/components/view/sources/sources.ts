73a7ae9fbb92950e21f9b32905a51b3e9eda9ce3
ulikemyway1 <calium@outlook.com>
2024-02-21 17:40:16 +0300

    refactor: avoid using ! ? and as operators in sources.ts file

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -2,19 +2,26 @@ import './sources.css';
 
 class Sources {
     public draw(data: { id: string; name: string }[]) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp')!;
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
 
         data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
-
-            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
-            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
+            if (sourceItemTemp) {
+                const sourceItemTempContent = sourceItemTemp.content.cloneNode(true);
+                if (sourceItemTempContent instanceof DocumentFragment) {
+                    const sourceClone = sourceItemTempContent;
+                    if (sourceClone) {
+                        const sourceCloneItemName = sourceClone.querySelector('.source__item-name');
+                        if (sourceCloneItemName) sourceCloneItemName.textContent = item.name;
+                        const sourceCloneItem = sourceClone.querySelector('.source__item');
+                        if (sourceCloneItem) sourceCloneItem.setAttribute('data-source-id', item.id);
+                        fragment.append(sourceClone);
+                    }
+                }
+            }
         });
-
-        document.querySelector('.sources')?.append(fragment);
+        const sources: HTMLElement | null = document.querySelector('.sources');
+        if (sources) sources.append(fragment);
     }
 }
 
