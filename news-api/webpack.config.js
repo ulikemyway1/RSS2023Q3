331f506885dd875786aa9ed1580ad7f262f7dc3e
ulikemywa1 <calium@outlook.com>
2024-02-26 21:03:40 +0300

    fix: change news-placeholder img and add webpack copy plugin

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -36,6 +37,9 @@ const baseConfig = {
         }),
         new CleanWebpackPlugin(),
         new EslingPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/img', to: 'img', noErrorOnMissing: true }],
+        }),
     ],
 };
 
