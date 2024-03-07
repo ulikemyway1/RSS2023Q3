bf70687978f0e9cb13cc3ca059284a5337d065f3
ulikemywa1 <calium@outlook.com>
2024-03-07 17:10:54 +0300

    feat: add more attributes to InputElement class

--- rss-puzzle/src/assets/utils/InputElement.ts
+++ rss-puzzle/src/assets/utils/InputElement.ts
@@ -1,31 +1,38 @@
-import BaseElement from "./baseElement"
-
+import BaseElement from './BaseElement';
 
 export default class InputElement extends BaseElement {
-    private type: string
+    private type: string;
 
-    private palceholder: string | undefined
+    private palceholder: string | undefined;
+
+    private value: string | undefined;
+
+    element: HTMLElement;
 
     constructor(
         type: string,
+        value?: string,
         placeholder?: string,
         id?: string,
-        classNames?: string[]
+        classNames?: string[],
+        textContent?: string
     ) {
-        super('input', id, classNames)
-        this.type = type
-        this.palceholder = placeholder
-        this.element = this.createInput()
+        super('input', id, classNames, textContent);
+        this.type = type;
+        this.value = value;
+        this.palceholder = placeholder;
+        this.element = this.createInput();
     }
 
     createInput() {
-        this.element.setAttribute('type', this.type)
+        this.element.setAttribute('type', this.type);
         if (this.palceholder)
-            this.element.setAttribute('placeholder', this.palceholder)
-        return this.element
+            this.element.setAttribute('placeholder', this.palceholder);
+        if (this.value) this.element.setAttribute('value', this.value);
+        return this.element;
     }
 
     getElement() {
-        return this.element
+        return this.element;
     }
 }
