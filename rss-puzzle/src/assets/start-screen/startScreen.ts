e2dc410bb39bc3173a62379c29cb4aac008f12a6
ulikemywa1 <calium@outlook.com>
2024-03-09 20:17:48 +0300

    feat: create Start Screen

---
+++ rss-puzzle/src/assets/start-screen/startScreen.ts
@@ -0,0 +1,50 @@
+import BaseElement from '../utils/BaseElement';
+import ParagraphElement from '../utils/ParagraphElement';
+import './startScreen.scss';
+
+type UserInfo = {
+    firstName: string;
+    lastName: string;
+};
+
+export default class StartScreen {
+    private content: HTMLElement | null;
+
+    private userInfo;
+
+    constructor(userInfo: UserInfo) {
+        this.userInfo = userInfo;
+        this.content = this.init();
+    }
+
+    private init() {
+        const wrapper = new BaseElement('div', undefined, [
+            'start-screen__wrapper',
+        ]).getElement();
+        const startScreen = new BaseElement('main', 'main', [
+            'start-screen',
+        ]).getElement();
+        const gameTitle = new BaseElement(
+            'h1',
+            undefined,
+            ['start-screen__game-title'],
+            'RSS Puzzle'
+        ).getElement();
+        const gameDescrtxt = `Learn English in a playful way. Discover the world's masterpieces by collecting sentences like puzzles.`;
+        const gameDescr = new ParagraphElement(gameDescrtxt, undefined, [
+            'start-screen__game-descr',
+        ]).getElement();
+        const userGreeting = new ParagraphElement(
+            `Welcome, ${this.userInfo.firstName} ${this.userInfo.lastName}!`,
+            undefined,
+            ['start-screen__greeting']
+        ).getElement();
+        wrapper.append(gameTitle, userGreeting, gameDescr);
+        startScreen.append(wrapper);
+        return startScreen;
+    }
+
+    getContent() {
+        return this.content;
+    }
+}
