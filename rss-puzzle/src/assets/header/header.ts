1dbc259982ee5dd6bec0e6b5b9f2a1b51fdb51a5
ulikemywa1 <calium@outlook.com>
2024-03-10 11:10:32 +0300

    feat: create header component

---
+++ rss-puzzle/src/assets/header/header.ts
@@ -0,0 +1,38 @@
+import appLoader from '../..';
+import BaseElement from '../utils/BaseElement';
+import InputElement from '../utils/InputElement';
+import deleteUser from '../utils/deleteUser';
+import './header.scss';
+
+export default class Header {
+    header: HTMLElement | null = null;
+
+    constructor() {
+        this.header = new BaseElement('header', undefined, [
+            'header',
+        ]).getElement();
+    }
+
+    private init() {
+        const logOutBtn = new InputElement(
+            'button',
+            'Logout',
+            undefined,
+            undefined,
+            ['button', 'logout-btn']
+        ).getElement();
+        logOutBtn.addEventListener('click', () => {
+            deleteUser();
+            while (document.body.lastChild) document.body.lastChild.remove();
+            appLoader.load();
+        });
+        if (this.header) {
+            this.header.append(logOutBtn);
+        }
+    }
+
+    getElement() {
+        this.init();
+        return this.header;
+    }
+}
