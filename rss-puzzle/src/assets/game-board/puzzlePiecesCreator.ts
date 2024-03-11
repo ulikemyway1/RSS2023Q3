406c4f3241f1f16cceacf40560e2e8aae99234f6
ulikemywa1 <calium@outlook.com>
2024-03-11 19:10:17 +0300

    feat: implement generation puzzle pieces from a sentence

---
+++ rss-puzzle/src/assets/game-board/puzzlePiecesCreator.ts
@@ -0,0 +1,47 @@
+import BaseElement from '../utils/BaseElement';
+import gameBoard from './gameBoard';
+import movePiece from './movePiece';
+
+export default class PuzzlePiecesCreator {
+    private sentence;
+
+    constructor(sentence: string) {
+        this.sentence = sentence;
+    }
+
+    public getPieces() {
+        const words = this.sentence.split(' ');
+        const puzzles: HTMLElement[] = [];
+        words.forEach((word) => {
+            const piece = new BaseElement(
+                'span',
+                undefined,
+                ['puzzle-piece'],
+                word
+            ).getElement();
+            piece.addEventListener('click', () => {
+                if (piece.parentElement === gameBoard.getSourceBlock()) {
+                    const dist = gameBoard.getResultBlock();
+                    if (dist) movePiece(piece, dist )
+                } 
+            })
+            puzzles.push(piece);
+        });
+        const tempArr: (null | HTMLElement | undefined)[] = Array(
+            puzzles.length * 50
+        ).fill(null);
+        while (puzzles.length > 0) {
+            const index = Math.floor(Math.random() * tempArr.length);
+            if (!tempArr[index]) tempArr[index] = puzzles.pop();
+        }
+        const randomPieceArray = tempArr.filter((item) => item);
+        const puzzlePieces: HTMLElement[] = [];
+        randomPieceArray.forEach((item) => {
+            if (item instanceof HTMLElement) {
+                puzzlePieces.push(item);
+            }
+        });
+
+        return puzzlePieces;
+    }
+}
