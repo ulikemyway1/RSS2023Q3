77ca7a71ad6fb2ff0cf66223e564f2908e1da5b1
ulikemywa1 <calium@outlook.com>
2024-03-13 15:29:15 +0300

    fix: fix to correctly move puzzle pieces

--- rss-puzzle/src/assets/game-board/movePiece.ts
+++ rss-puzzle/src/assets/game-board/movePiece.ts
@@ -1,3 +1,21 @@
 export default function movePiece(elem: HTMLElement, dist: Element): void {
-    dist.append(elem);
+    if (dist.classList.contains('game-board__source-block')) {
+        dist.append(elem);
+    } else {
+        let fisrtFreeSpace: Element | null = null;
+        if (dist.firstElementChild && dist.firstElementChild.firstElementChild) {
+            let currentElementChild: ChildNode | Element  = dist.firstElementChild.firstElementChild;
+            for (let i = 1; i <= dist.firstElementChild.childElementCount; i += 1) {
+                if (!currentElementChild.firstChild && currentElementChild instanceof Element) {
+                    fisrtFreeSpace = currentElementChild;
+                    break;
+                }
+                if (currentElementChild.nextSibling)
+                currentElementChild = currentElementChild.nextSibling;
+
+            }
+        }
+        if (fisrtFreeSpace) fisrtFreeSpace.append(elem);
+    }
+
 }
