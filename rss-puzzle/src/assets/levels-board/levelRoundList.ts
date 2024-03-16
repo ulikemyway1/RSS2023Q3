3788bf379dff888eac26f521b998891d76098fb8
ulikemywa1 <calium@outlook.com>
2024-03-16 20:48:17 +0300

    feat: implement LevelRoundList class

---
+++ rss-puzzle/src/assets/levels-board/levelRoundList.ts
@@ -0,0 +1,28 @@
+import { IwordCollectionData } from '../game-board/gameBoard';
+import ListElement from '../utils/ListElement';
+import Levels from './levelsSrc';
+
+export default class LevelRoundList {
+    dataSourceLink: Levels;
+
+    constructor(src: Levels) {
+        this.dataSourceLink = src;
+    }
+
+    private async fetchData() {
+        const levelData: IwordCollectionData = await fetch(this.dataSourceLink)
+            .then((respone) => respone.json())
+            .then((data) => data);
+        const list = new ListElement('ol', ['levels-board__level-list']);
+
+        levelData.rounds.forEach((round) => {
+            list.addListItem(round.levelData.name, ['level-list__item']);
+        });
+        return list;
+    }
+
+    public async getList() {
+        const list = await this.fetchData().then((list) => list.getList());
+        return list;
+    }
+}
