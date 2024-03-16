dff9aab41835300f8b736bd885ad8d897a4947d2
ulikemywa1 <calium@outlook.com>
2024-03-16 20:47:10 +0300

    feat: create Levels enum with url to Level Data

---
+++ rss-puzzle/src/assets/levels-board/levelsSrc.ts
@@ -0,0 +1,10 @@
+enum Levels {
+    'Level-1' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel1.json`,
+    'Level-2' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel2.json`,
+    'Level-3' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel3.json`,
+    'Level-4' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel4.json`,
+    'Level-5' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel5.json`,
+    'Level-6' = `https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel6.json`,
+}
+
+export default Levels;
