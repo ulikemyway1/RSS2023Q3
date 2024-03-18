8a063b502daa4343a550b6a65693e39514edb84c
ulikemyway1 <calium@outlook.com>
2024-03-19 02:06:22 +0300

    feat: add favicon, change title (#95)

--- rss-puzzle/webpack.config.cjs
+++ rss-puzzle/webpack.config.cjs
@@ -39,7 +39,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'RSS Puzzle',
+        }),
         new CopyPlugin({
             patterns: [
                 { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
