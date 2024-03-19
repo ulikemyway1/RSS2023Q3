23cb1e30614d4e639a8286d4955f83d219300d2c
ulikemywa1 <calium@outlook.com>
2024-03-19 20:47:58 +0300

    fix: reset gameboard after using statistics and logout

--- rss-puzzle/src/assets/utils/deleteUser.ts
+++ rss-puzzle/src/assets/utils/deleteUser.ts
@@ -8,4 +8,9 @@ export default function deleteUser() {
     localStorage.removeItem('lastCompleted__ULIKE');
     gameProgressObserver.deleteAllData();
     gameBoard.statisticObserver.reset();
+    gameBoard.levelNumber = 1;
+    gameBoard.roundNumber = 0;
+    gameBoard.wordNumber = 0;
+    gameBoard.statisticBtn.classList.add('hidden');
+    gameBoard.autoCompleteBtn.disabled = false;
 }
