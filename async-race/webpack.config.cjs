a0b78aced0f68377ca606157973066cb880739a4
ulikemywa1 <calium@outlook.com>
2024-03-21 21:16:21 +0300

    feat: add base template

--- async-race/webpack.config.cjs
+++ async-race/webpack.config.cjs
@@ -1,55 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true },
-                    },
-                ],
-            },
+  entry: "./src/index.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'AsyncRACE',
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
-            ],
-        }),
+      },
     ],
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "AsyncRACE",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets", noErrorOnMissing: true }],
+    }),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 
-    devtool: 'source-map',
+  devtool: "source-map",
 };
