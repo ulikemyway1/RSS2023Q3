83b4d28aa1d3401450975baddb2ad4737cda64d1
ulikemywa1 <calium@outlook.com>
2024-03-23 19:12:12 +0300

    feat: implement deleteCar method

--- async-race/src/garage/garageDataOwner.ts
+++ async-race/src/garage/garageDataOwner.ts
@@ -12,6 +12,12 @@ class GarageDataOwner {
     const data: AllCardDescr = await response.json();
     return data;
   }
+
+  async deleteCar(id: number) {
+    await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+      method: "DELETE",
+    });
+  }
 }
 
 const garageDataOwner = new GarageDataOwner("http://127.0.0.1:3000/garage");
