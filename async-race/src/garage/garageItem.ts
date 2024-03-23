d4bb4e0394566d9d1b226b068f0385b9e332ccfa
ulikemywa1 <calium@outlook.com>
2024-03-23 18:01:26 +0300

    feat: implement canceling and applying changes of car attr

--- async-race/src/garage/garageItem.ts
+++ async-race/src/garage/garageItem.ts
@@ -17,12 +17,22 @@ export default class GarageItem {
 
   private carName: string;
 
+  private editModeActive = false;
+
+  private currentCarName: string;
+
+  private currentCarColor: string;
+
   constructor(carDescr: CarDescr) {
     this.car = new Car(carDescr.id, carDescr.color, carDescr.name);
     this.colorSelector.bindElement(this.car);
     this.colorSelector.setInitialColor(carDescr.color);
+    this.colorSelector.getColorSelector().disabled = true;
+    this.nameSelector.getNameSelector().disabled = true;
     this.carName = carDescr.name;
     this.nameSelector.bindCar(this.car);
+    this.currentCarColor = carDescr.color;
+    this.currentCarName = carDescr.name;
     const wrapper = new BaseElement("div", [
       "garage__item-wrapper",
     ]).getElement();
@@ -52,11 +62,42 @@ export default class GarageItem {
     ).getElement();
 
     btnWrapper.append(editBtn, applyBtn);
+
+    editBtn.addEventListener("click", () => {
+      if (!this.editModeActive && this.car) {
+        editBtn.textContent = "Cancel";
+        applyBtn.classList.remove("hidden");
+        this.editModeActive = true;
+        this.colorSelector.getColorSelector().disabled = false;
+        this.nameSelector.getNameSelector().disabled = false;
+      } else {
+        editBtn.textContent = "Edit";
+        applyBtn.classList.add("hidden");
+        this.editModeActive = false;
+        this.colorSelector.getColorSelector().disabled = true;
+        this.nameSelector.getNameSelector().disabled = true;
+        if (this.car) {
+          this.nameSelector.getNameSelector().value = this.currentCarName;
+          this.colorSelector.setInitialColor(this.currentCarColor);
+          this.car
+            .getCar()
+            .style.setProperty("--car-color", this.currentCarColor);
+        }
+      }
+    });
+
     applyBtn.addEventListener("click", () => {
       if (this.car) {
         const carID = this.car.getID();
         console.log(carID);
         this.editCar(carID);
+        editBtn.textContent = "Edit";
+        applyBtn.classList.add("hidden");
+        this.editModeActive = false;
+        this.colorSelector.getColorSelector().disabled = true;
+        this.nameSelector.getNameSelector().disabled = true;
+        this.currentCarColor = this.nameSelector.getNameSelector().value;
+        this.currentCarName = this.colorSelector.getColorSelector().value;
       }
     });
     this.item.append(this.car.getCar(), wrapper, btnWrapper, deleteBtn);
