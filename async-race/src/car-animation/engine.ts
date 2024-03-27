d1305e52abfbbac78c5c2baa69618e643b1f8c83
ulikemywa1 <calium@outlook.com>
2024-03-27 14:04:43 +0300

    feat: handle more errors

--- async-race/src/car-animation/engine.ts
+++ async-race/src/car-animation/engine.ts
@@ -1,10 +1,13 @@
 import Car from "../garage/car";
+import GarageItem from "../garage/garageItem";
 
 export default class Engine {
   car: Car;
   carView: HTMLElement;
+  context: GarageItem;
   constructor(car: Car) {
     this.car = car;
+    this.context = this.car.context;
     this.carView = this.car.getCar();
   }
   async start() {
@@ -16,6 +19,7 @@ export default class Engine {
     )
       .then((response) => response.json())
       .then((data) => this.applyDrivingStyles(data))
+      .then(() => (this.context.driveBtn.disabled = true))
       .then(() => this.drive());
   }
 
@@ -29,16 +33,40 @@ export default class Engine {
   }
 
   private async drive() {
-    await fetch(
-      `http://127.0.0.1:3000/engine?id=${this.car.getID()}&status=drive`,
-      {
-        method: "PATCH",
-      },
-    ).then((response) => {
-      if (response.status === 500) {
-        this.carView.classList.add("stop-driving");
+    this.context.stopBtn.disabled = false;
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:3000/engine?id=${this.car.getID()}&status=drive`,
+        {
+          method: "PATCH",
+        },
+      ).then((response) => {
+        if (response.status === 500) {
+          this.carView.classList.add("stop-driving");
+          throw new Error(
+            `Car (ID ${this.car.getID()} - ${this.car.getName()}) has been stopped suddenly. It's engine was broken down.`,
+          );
+        } else if (response.status === 429) {
+          throw new Error(
+            `Car (ID ${this.car.getID()} - ${this.car.getName()}) is already driving`,
+          );
+        } else if (response.status === 404) {
+          throw new Error(
+            `Car (ID ${this.car.getID()} - not found or its engine is not started`,
+          );
+        } else if (response.status === 400) {
+          throw new Error(
+            `Car (ID ${this.car.getID()} - Ooops. Wrong paramets: "status" should be "started", "stopped" or "drive"`,
+          );
+        }
+      });
+    } catch (e) {
+      if (e instanceof Error) {
+        console.warn(e.message);
+      } else {
+        console.warn(e);
       }
-    });
+    }
   }
 
   public stopDriving() {
@@ -47,6 +75,13 @@ export default class Engine {
       {
         method: "PATCH",
       },
-    ).then(() => this.car.getCar().classList.remove("driving", "stop-driving"));
+    )
+      .then(() => this.car.getCar().classList.remove("driving", "stop-driving"))
+      .then(() => {
+        this.context.driveBtn.disabled = false;
+        this.context.stopBtn.disabled = true;
+        this.context.deleteBtn.disabled = false;
+        this.context.editBtn.disabled = false;
+      });
   }
 }
