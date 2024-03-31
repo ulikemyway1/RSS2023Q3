86ab297b6ac25da33a2b1eb14ed3ecf6e026b83b
ulikemyway1 <calium@outlook.com>
2024-03-31 16:23:18 +0300

    Implement main view (#105)
    * feat: implement main view
    
    * feat: add some content to the main page

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -2,9 +2,23 @@ import Car from "./garage/car";
 import CarsCounter from "./garage/carsCounter";
 import ColorSelector from "./garage/colorSelector";
 import GarageView from "./garage/garageView";
+import MainVeiw from "./main/mainView";
 import "./style.scss";
 import WinnersView from "./winners/winnersView";
 const garage = new GarageView();
 const winners = new WinnersView();
+const mainVeiw = new MainVeiw();
+
+mainVeiw.addContent([
+  {
+    name: "Open Garage",
+    element: garage.getView(),
+  },
+  {
+    name: "Open Winners Table",
+    element: winners.getView(),
+  },
+]);
+
+document.body.append(mainVeiw.getHeader(), mainVeiw.getMainConent());
 export default garage;
-document.body.append(garage.getView(), winners.getView());
---
+++ async-race/src/main/mainView.ts
@@ -0,0 +1,77 @@
+import ButtonElement from "../utils/InputElement";
+import BaseElement from "../utils/baseElement";
+import startPage from "./mainPage";
+import "./mainView.scss";
+
+export default class MainVeiw {
+  content: HTMLElement[] = [];
+
+  header: HTMLElement = new BaseElement("header", ["header"]).getElement();
+
+  nav: HTMLElement = new BaseElement("nav", ["nav"]).getElement();
+
+  navUl: HTMLElement = new BaseElement("ul", ["header__nav-ul"]).getElement();
+
+  btnControls: HTMLButtonElement[] = [];
+
+  contentContainer: HTMLElement = new BaseElement("main", [
+    "main",
+  ]).getElement();
+
+  mainPage: HTMLElement = startPage.getView();
+
+  constructor() {
+    this.nav.append(this.navUl);
+    this.header.append(this.nav);
+    this.content.push(this.mainPage);
+    this.contentContainer.append(this.mainPage);
+
+    const mainViewBtn = new ButtonElement(
+      ["main-btn", "button"],
+      "Main Page",
+    ).getButton();
+    mainViewBtn.disabled = true;
+    mainViewBtn.addEventListener("click", (e) => {
+      this.btnControls.forEach((btn) => (btn.disabled = false));
+      if (e.target === mainViewBtn) {
+        mainViewBtn.disabled = true;
+      }
+      this.content.forEach((element) => element.remove());
+      this.contentContainer.append(this.mainPage);
+    });
+    this.btnControls.push(mainViewBtn);
+    this.navUl.append(mainViewBtn);
+  }
+
+  public addContent(content: { name: string; element: HTMLElement }[]): void {
+    content.forEach((item) => {
+      this.createNavItem(item.name, item.element);
+      this.content.push(item.element);
+    });
+  }
+
+  public getHeader(): HTMLElement {
+    return this.header;
+  }
+
+  public getMainConent(): HTMLElement {
+    return this.contentContainer;
+  }
+
+  private createNavItem(textContent: string, target: HTMLElement): void {
+    const btn = new ButtonElement(
+      ["header__nav-ul__li", "button"],
+      textContent,
+    ).getButton();
+    btn.addEventListener("click", (e) => {
+      this.btnControls.forEach((btn) => (btn.disabled = false));
+      if (e.target === btn) {
+        btn.disabled = true;
+      }
+      this.content.forEach((element) => element.remove());
+      this.contentContainer.append(target);
+    });
+    this.btnControls.push(btn);
+    this.navUl.append(btn);
+  }
+}
