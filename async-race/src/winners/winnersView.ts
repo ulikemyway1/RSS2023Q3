26621d190291d71935c718e6b42f97d53b6da995
ulikemywa1 <calium@outlook.com>
2024-03-31 13:41:29 +0300

    feat: add winners table header

--- async-race/src/winners/winnersView.ts
+++ async-race/src/winners/winnersView.ts
@@ -3,6 +3,7 @@ import { IWinnersTableItem } from "../types/interface";
 import BaseElement from "../utils/baseElement";
 import WinnersItemWrapper from "./winnersItemWrapper";
 import WinnersTableItem, { winnerInfo } from "./winnersTableItem";
+import ButtonElement from "../utils/InputElement";
 
 export default class WinnersView {
   private view: HTMLElement = new BaseElement(
@@ -15,11 +16,17 @@ export default class WinnersView {
 
   private itemsWrapper: WinnersItemWrapper | null = null;
 
+  private header: HTMLElement = new BaseElement("div", [
+    "winners__header",
+  ]).getElement();
+
   constructor() {
+    this.initHeader();
     this.loadRecords()
       .then(() => (this.itemsWrapper = new WinnersItemWrapper(this.content)))
       .then(() => {
-        if (this.itemsWrapper) this.view.append(this.itemsWrapper.getView());
+        if (this.itemsWrapper)
+          this.view.append(this.header, this.itemsWrapper.getView());
       });
   }
 
@@ -40,5 +47,38 @@ export default class WinnersView {
   private createTableItem(descr: winnerInfo, index: number): WinnersTableItem {
     const winnerTableitem = new WinnersTableItem(descr, index + 1);
     return winnerTableitem;
+  }
+
+  private initHeader() {
+    const carIndexHeader = new BaseElement(
+      "span",
+      ["car-index-header"],
+      "№",
+    ).getElement();
+    const carImgHeader = new BaseElement(
+      "span",
+      ["car-img-header"],
+      "Car",
+    ).getElement();
+    const carNameHeader = new BaseElement(
+      "span",
+      ["car-name-header"],
+      "Name",
+    ).getElement();
+    const carWinsHeader = new ButtonElement(
+      ["car-wins-header", "button"],
+      "Wins",
+    ).getButton();
+    const carTimeHeader = new ButtonElement(
+      ["car-time-header", "button"],
+      "Time",
+    ).getButton();
+    this.header.append(
+      carIndexHeader,
+      carImgHeader,
+      carNameHeader,
+      carWinsHeader,
+      carTimeHeader,
+    );
   }
 }
