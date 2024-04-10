166c0a1e81a15e9b2f2a7278b9941fe099f4c690
ulikemywa1 <calium@outlook.com>
2024-04-10 15:13:26 +0300

    init: start project

---
+++ fun-chat/.eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'airbnb',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'import/no-unresolved': [2, { caseSensitive: false }],
+        'no-shadow': 'off',
+        'class-methods-use-this': 'off',
+        'import/no-extraneous-dependencies': [
+            'warn',
+            { devDependencies: ['**/*.test.js', '**/*.spec.js'] },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+            'import/extensions': {
+                'import/extensions': 'never',
+            },
+        },
+    },
+};
