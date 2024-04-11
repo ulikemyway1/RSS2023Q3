5a5989e85426294cc8eaf38c931d7508e75eefec
ulikemywa1 <calium@outlook.com>
2024-04-11 15:28:22 +0300

    feat: add style for validation

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -5,5 +5,5 @@ const ws = new WebSocket('ws://localhost:4000/');
 ws.addEventListener('open', () => {
     console.log('Connected');
 });
-
+export default ws;
 document.body.append(new LoginPage().getLoginPage());
