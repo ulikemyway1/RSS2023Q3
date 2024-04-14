4cb5bae5b53e3e7fcae0a7420c725a84578b5f0a
ulikemywa1 <calium@outlook.com>
2024-04-14 15:29:37 +0300

    feat: implement App class

---
+++ fun-chat/src/app/app.ts
@@ -0,0 +1,33 @@
+import Router from '../router/Router';
+import routes from '../router/routes';
+import State from '../state/State';
+import BaseElement from '../utils/BaseElement';
+
+class App {
+    private container = new BaseElement('div', ['container']).getElement();
+    private state = new State();
+    private router = new Router(routes);
+
+    public start(): void {
+        document.body.append(this.container);
+        this.router.navigate('login');
+    }
+
+    public setContent(content: HTMLElement) {
+        while (this.container.lastElementChild) {
+            this.container.lastElementChild.remove();
+        }
+        this.container.append(content);
+    }
+
+    public getRouter() {
+        return this.router;
+    }
+
+    public getState() {
+        return this.state;
+    }
+}
+
+const app = new App();
+export default app;
