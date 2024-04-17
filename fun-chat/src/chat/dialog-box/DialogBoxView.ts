fd91e0f8ff833d05a6b214a7facaa992445d65db
ulikemywa1 <calium@outlook.com>
2024-04-17 13:43:40 +0300

    feat: create  dialog box main elements

--- fun-chat/src/chat/dialog-box/DialogBoxView.ts
+++ fun-chat/src/chat/dialog-box/DialogBoxView.ts
@@ -1,7 +1,40 @@
 import BaseElement from '../../utils/BaseElement';
+import ButtonElement from '../../utils/ButtonElement';
+import InputElement from '../../utils/InputElement';
 import './dialogBox.scss';
 class DialogBoxView {
     private view = new BaseElement('section', ['dialog-box']).getElement();
+
+    private header = new BaseElement('div', [
+        'dialog-box__header',
+    ]).getElement();
+
+    private msgArea = new BaseElement(
+        'div',
+        ['dialog-box__msg-area'],
+        'Choose a contact to start chating'
+    ).getElement();
+
+    private inputWrapper = new BaseElement('div', [
+        'dialog-box__input-wrapper',
+    ]).getElement();
+
+    private inputField = document.createElement('textarea');
+
+    private sendMessageBtn = new ButtonElement(
+        'Send',
+        ['button', 'send-btn'],
+        true
+    ).getButton();
+
+    constructor() {
+        this.inputField.classList.add('dialog-box__input-field');
+        this.inputField.disabled = true;
+        this.inputField.placeholder = 'Type here...';
+        this.inputWrapper.append(this.inputField, this.sendMessageBtn);
+
+        this.view.append(this.header, this.msgArea, this.inputWrapper);
+    }
 
     public getView() {
         return this.view;
