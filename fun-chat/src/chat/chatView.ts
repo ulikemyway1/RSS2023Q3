8dfbc907a7560f29c82964058be1acb9651cd00e
ulikemywa1 <calium@outlook.com>
2024-04-22 20:02:53 +0300

    feat: implement adaptive design -- hide contacts

--- fun-chat/src/chat/chatView.ts
+++ fun-chat/src/chat/chatView.ts
@@ -2,12 +2,27 @@ import BaseElement from '../utils/BaseElement';
 import './chat.scss';
 import dialogBoxView from './dialog-box/DialogBoxView';
 import userListView from './contacts-list/ContactsListView';
+import ButtonElement from '../utils/ButtonElement';
 
 class ChatView {
     private chatView = new BaseElement('section', ['chat']).getElement();
 
+    private showContactsBtn = new ButtonElement(
+        'Contacts',
+        ['show-contacts-btn'],
+        false
+    ).getButton();
+
     constructor() {
-        this.chatView.append(userListView.getView(), dialogBoxView.getView());
+        this.showContactsBtn.addEventListener('click', () => {
+            userListView.getView().classList.toggle('open');
+            dialogBoxView.getView().classList.toggle('closed');
+        });
+        this.chatView.append(
+            this.showContactsBtn,
+            userListView.getView(),
+            dialogBoxView.getView()
+        );
     }
 
     public getChatView() {
