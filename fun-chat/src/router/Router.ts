499c4e1f07aea52aa259a684ac2f9c7f673efe36
ulikemyway1 <calium@outlook.com>
2024-05-04 11:19:33 +0300

    Fun chat  feat/improve routing (#129)
    * feat: add current route in the browser line
    
    * fir: correct path

--- fun-chat/src/router/Router.ts
+++ fun-chat/src/router/Router.ts
@@ -6,10 +6,18 @@ export default class Router {
 
     constructor(routes: RoutesDescription) {
         this.routes = routes;
+        window.addEventListener('popstate', (e) => {
+            e.preventDefault();
+            this.navigate(window.location.pathname.replace('/', ''));
+        });
+        window.addEventListener('load', () => {
+            this.navigate(window.location.pathname.replace('/', ''));
+        });
     }
 
     public navigate(path: string) {
         if (this.routes[path]) {
+            history.pushState({}, '', path);
             app.setContent(this.routes[path].getView());
         } else {
             app.setContent(this.routes['notFound'].getView());
